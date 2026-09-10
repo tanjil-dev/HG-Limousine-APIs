99df445941b35fa715872066080e498df028034b
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2026-09-10 18:40:18 +0600

    django admin title

--- core_api/admin.py
+++ core_api/admin.py
@@ -4,3 +4,7 @@ from .models import CarModel
 
 
 admin.site.register(CarModel)
+
+admin.site.site_header = "H&G Limousine"
+admin.site.site_title = "H&G Limousine Admin"
+admin.site.index_title = "H&G Limousine Dashboard"
